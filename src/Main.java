1112534003a865368a0f899e2cbada19c54bccaa
Austin Long <austin@austinlong.dev>
2025-09-06 15:25:48 -0500

    Wrote program

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter your first name: ");
+    String firstName = scanner.nextLine();
+
+    System.out.print("Enter your last name: ");
+    String lastName = scanner.nextLine();
+
+    System.out.print("Enter your age: ");
+    int age = scanner.nextInt();
+    scanner.nextLine(); // Flush the leftover newline from STDIN
+
+    System.out.print(
+      "Enter the average amount of sleep you get in a day in hours: "
+    );
+    float avgSleep = scanner.nextFloat();
+    scanner.nextLine();
+
+    System.out.println("Person");
+    System.out.println("------------------------------");
+    System.out.format("%-15s%15s\n", "First name:", firstName);
+    System.out.format("%-15s%15s\n", "Last name:", lastName);
+    System.out.format("%-15s%9d years\n", "Age:", age);
+    System.out.format("%-20s%4.1f hours\n", "Avg daily sleep:", avgSleep);
+
+    scanner.close();
+  }
 }
